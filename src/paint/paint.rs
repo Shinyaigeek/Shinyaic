cbaf3f49420ba35c55046cbe51e722de2112c799
Shinyaigeek <Philispaxil@gmail.com>
2021-09-22 17:15:57 +0900

    Refactor: background-color getting way

--- src/paint/paint.rs
+++ src/paint/paint.rs
@@ -42,8 +42,33 @@ fn prepare() -> RenderTree {
 <meta charset=\"utf-8\" />
 <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />  
+<style type=\"text/css\">
+body {
+    background-color: #f0f0f2;
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;
+}
+div {
+    width: 600px;
+    margin: 5em auto;
+    padding: 2em;
+    background-color: #fdfdff;
+    border-radius: 0.5em;
+    box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+}
+a:link, a:visited {
+    color: #38488f;
+    text-decoration: none;
+}
+@media (max-width: 700px) {
+    div {
+        margin: 0 auto;
+        width: auto;
+    }
+}
+</style>
 </head>
-
 <body>
 <div>
     <h1>Example Domain</h1>
@@ -167,12 +192,21 @@ impl Sandbox for Window {
                 | RenderObject::Scroll(rendering_object) => {
                     let mut background_color = Color::new(1.0, 1.0, 1.0, 1.0);
                     for style in &rendering_object.style {
-                        if style.declarations.get(&"background".to_string()).is_some() {
-                            let mut raw_background_color = style
+                        let background_color_value = {
+                            if style.declarations.get(&"background".to_string()).is_some() {
+                                style.declarations.get(&"background".to_string())
+                            } else if style
                                 .declarations
-                                .get(&"background".to_string())
-                                .unwrap()
-                                .clone();
+                                .get(&"background-color".to_string())
+                                .is_some()
+                            {
+                                style.declarations.get(&"background-color".to_string())
+                            } else {
+                                None
+                            }
+                        };
+                        if background_color_value.is_some() {
+                            let mut raw_background_color = background_color_value.unwrap().clone();
                             raw_background_color.retain(|c| {
                                 c == ','
                                     || c == '.'
